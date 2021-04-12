37d0b1a92b69c4903c4f3be4b783d02a555b7ec5
MaximBrewer <pimax1978@icloud.com>
2021-04-12 15:26:27 +0600

    fix

--- components/UrlManager.php
+++ components/UrlManager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: resh
@@ -22,63 +23,62 @@ class UrlManager extends \yii\web\UrlManager
     {
         $url = parent::createUrl($params);
         $page = Page::find()->where(['route' => $url])->one();
-        if($page) {
+        if ($page) {
             return $page->slug;
         }
 
-	    if($params[0] == 'category/index'){
-	    	$category = Category::findOne($params['id']);
-	    	if($category){
-	    		unset($params[0]);
-	    		$url = $category->slug;
-	    		if(isset($params['page'])){
-	    			$url .= '?page=' . $params['page'];
-			    }
-		    }
-	    }
+        if ($params[0] == 'category/index') {
+            $category = Category::findOne($params['id']);
+            if ($category) {
+                unset($params[0]);
+                $url = $category->slug;
+                if (isset($params['page'])) {
+                    $url .= '?page=' . $params['page'];
+                }
+            }
+        }
 
         return $url;
-
     }
 
     public function parseRequest($request)
     {
         $path = $request->getPathInfo();
         $path = trim($path, '/');
-        if(strpos($path, 'news') === 0){
+        if (strpos($path, 'news') === 0) {
             $page = News::find()->where(['slug' => '/' . $path])->one();
-            if($page){
+            if ($page) {
                 \Yii::$app->params['page'] = $page;
-                return ['/news' , ['id' => $page->id]];
+                return ['/news', ['id' => $page->id]];
             }
         }
-        if(strpos($path, 'category') === 0){
+        if (strpos($path, 'category') === 0) {
             $page = Category::find()->where(['slug' => '/' . $path])->one();
-            if($page){
+            if ($page) {
                 \Yii::$app->params['page'] = $page;
-                return ['/category' , ['id' => $page->id]];
+                return ['/category', ['id' => $page->id]];
             }
         }
-        if(strpos($path, 'product') === 0){
+        if (strpos($path, 'product') === 0) {
             $page = Products::find()->where(['slug' => '/' . $path])->one();
-            if($page){
+            if ($page) {
                 \Yii::$app->params['page'] = new Page();
-                return ['/product' , ['id' => $page->id]];
+                return ['/product', ['id' => $page->id]];
             }
         }
-        
+
         $seourl = SeoUrl::find()->where(['slug' => '/' . $path])->one();
-        if($seourl && $seourl->module == 'catalog'){
+        if ($seourl && $seourl->module == 'catalog') {
             \Yii::$app->params['page'] = $seourl;
             return [$seourl->route, ['id' => $seourl->item_id]];
         }
-        
-        $page = Page::find()->where(['slug' => '/' . $path])->one();
-        if($page){
-            \Yii::$app->params['page'] = $page;
-            return [$page->route, []];
+        if ($path != 'order') {
+            $page = Page::find()->where(['slug' => '/' . $path])->one();
+            if ($page) {
+                \Yii::$app->params['page'] = $page;
+                return [$page->route, []];
+            }
         }
         return parent::parseRequest($request);
     }
-
 }
