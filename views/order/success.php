40de764f5d680eb9375ee6239d1e68caa8bb0463
MaximBrewer <pimax1978@icloud.com>
2021-04-12 15:31:18 +0600

    fix

--- views/order/success.php
+++ views/order/success.php
@@ -3,6 +3,7 @@
  * @var $this \yii\web\View
  */
 $page = \app\models\Page::find()->where(['slug' => '/order'])->one();
+Yii::$app->params['page'] = $page;
 ?>
 <div class="container">
     <div class="order_success1">
