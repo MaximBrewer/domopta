46acf860a241c30c48de19ca135318faf5cc4f1e
MaximBrewer <pimax1978@icloud.com>
2021-05-20 19:17:16 +0600

    xsd

--- controllers/CabinetController.php
+++ controllers/CabinetController.php
@@ -63,7 +63,7 @@ class CabinetController extends Controller
 
 	public function actionCsv()
 	{
-		// \YII::$app->cache->flush();
+		\YII::$app->cache->flush();
 		$user = User::findOne(\Yii::$app->user->id);
 		if (!$user->profile->type) {
 			return $this->redirect(['reg/full?step=1']);
@@ -78,7 +78,7 @@ class CabinetController extends Controller
 
 	public function actionXml()
 	{
-		// \YII::$app->cache->flush();
+		\YII::$app->cache->flush();
 		$user = User::findOne(\Yii::$app->user->id);
 		if (!$user->profile->type) {
 			return $this->redirect(['reg/full?step=1']);
