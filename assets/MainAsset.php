f2f03763f73f947df579d9edc54f082f21efc053
MaximBrewer <pimax1978@icloud.com>
2021-05-21 09:42:50 +0600

    fix

--- assets/MainAsset.php
+++ assets/MainAsset.php
@@ -18,11 +18,11 @@ class MainAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'style/style.css?v=5.2.2',
+        'style/style.css?v=5.2.3',
         'css/jquery.jgrowl.min.css'
     ];
     public $js = [
-        'js/main.js?v=5.2.2',
+        'js/main.js?v=5.2.3',
         'js/jquery.mCustomScrollbar.js',
         'js/jquery.jgrowl.min.js',
 //        'js/common.js'
