1c274f6d6133f00778fc6c7d12b78e6aa8469c08
MaximBrewer <pimax1978@icloud.com>
2021-05-25 15:15:45 +0600

    fix

--- modules/adminka/views/bigcart/index.php
+++ modules/adminka/views/bigcart/index.php
@@ -6,60 +6,63 @@ use yii\grid\GridView;
 use yii\helpers\Html;
 ?>
 <div class="container">
-<div class="nouse">
-	<?php echo GridView::widget([
-		'dataProvider' => $dataProvider,
-		'columns' => [
-			[
-				'label' => 'ФИО',
-				'value' => function ($model) {
-					return $model->profile->lastname . ' ' . $model->profile->name . ' ' . $model->profile->surname;
-				}
-			],
-			[
-				'label' => 'Город',
-				'value' => function ($model) {
-					return $model->profile->city;
-				}
-			],
-			[
-				'label' => 'Область',
-				'value' => function ($model) {
-					return $model->profile->region;
-				}
-			],
-			[
-				'attribute' => 'username',
-				'label' => 'Телефон',
-				'value' => function ($model) {
-					if (\Yii::$app->user->identity->role == 'admin') {
-						return Html::a($model->username, ['/user/admin/update', 'id' => $model->id]);
-					} else {
-						return $model->username;
+	<div class="text-right">
+		<a href="/adminka/bigcart/xls" class="btn btn-primary">Реестр Корзина от 5000 (XLS)</a>
+	</div>
+	<div class="nouse">
+		<?php echo GridView::widget([
+			'dataProvider' => $dataProvider,
+			'columns' => [
+				[
+					'label' => 'ФИО',
+					'value' => function ($model) {
+						return $model->profile->lastname . ' ' . $model->profile->name . ' ' . $model->profile->surname;
 					}
-				},
-				'format' => 'raw'
-			],
-			[
-				'label' => 'Тип цен',
-				'value' => function ($model) {
-					return $model->profile->type == 2 ? 'Мелкий опт' : 'Опт';
-				}
-			],
-			[
-				'label' => 'Сумма',
-				'value' => function ($model) {
-					return $model->getCartSum();
-				}
-			],
-			[
-				'label' => 'Корзина',
-				'value' => function ($model) {
-					return Html::a("Смотреть содержимое корзины", ['/adminka/bigcart/cart', 'id' => $model->id]);
-				},
-				'format' => 'raw'
+				],
+				[
+					'label' => 'Город',
+					'value' => function ($model) {
+						return $model->profile->city;
+					}
+				],
+				[
+					'label' => 'Область',
+					'value' => function ($model) {
+						return $model->profile->region;
+					}
+				],
+				[
+					'attribute' => 'username',
+					'label' => 'Телефон',
+					'value' => function ($model) {
+						if (\Yii::$app->user->identity->role == 'admin') {
+							return Html::a($model->username, ['/user/admin/update', 'id' => $model->id]);
+						} else {
+							return $model->username;
+						}
+					},
+					'format' => 'raw'
+				],
+				[
+					'label' => 'Тип цен',
+					'value' => function ($model) {
+						return $model->profile->type == 2 ? 'Мелкий опт' : 'Опт';
+					}
+				],
+				[
+					'label' => 'Сумма',
+					'value' => function ($model) {
+						return $model->getCartSum();
+					}
+				],
+				[
+					'label' => 'Корзина',
+					'value' => function ($model) {
+						return Html::a("Смотреть содержимое корзины", ['/adminka/bigcart/cart', 'id' => $model->id]);
+					},
+					'format' => 'raw'
+				]
 			]
-		]
-	]) ?>
-</div>
+		]) ?>
+	</div>
 </div>
