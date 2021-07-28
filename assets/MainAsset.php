a053276522d99390ad1db71ad5a9425225b58698
MaximBrewer <pimax1978@icloud.com>
2021-07-28 22:10:38 +0600

    sd

--- assets/MainAsset.php
+++ assets/MainAsset.php
@@ -18,11 +18,11 @@ class MainAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'style/style.css?v=5.2.6',
+        'style/style.css?v=5.2.7',
         'css/jquery.jgrowl.min.css'
     ];
     public $js = [
-        'js/main.js?v=5.2.6',
+        'js/main.js?v=5.2.7',
         'js/jquery.mCustomScrollbar.js',
         'js/jquery.jgrowl.min.js',
 //        'js/common.js'
